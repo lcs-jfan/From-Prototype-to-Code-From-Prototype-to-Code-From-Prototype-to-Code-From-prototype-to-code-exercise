07f41efa2cee4f8b22a10dcf58fe184ba93db3c1
Yiming Fan <jfan@lcs.on.ca>
2026-02-26 09:32:29 -0500

    Finished "List"

--- From Prototype to Code From Prototype to Code From Prototype to Code From prototype to code exercise/ContentView.swift
+++ From Prototype to Code From Prototype to Code From Prototype to Code From prototype to code exercise/ContentView.swift
@@ -8,12 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var selection = "Planned Feedings"
+    
     var body: some View {
         NavigationStack{
+            //Picker(Bar Figure)
             VStack {
+                Picker("Currerent Selection", selection: $selection){
+                    Text("Meals").tag("Meals")
+                    Text("Planned Feedings").tag("Planned Feedings")
+                    Text("History").tag("History")
+                }
+                .pickerStyle(.segmented)
+                    
             }
             .padding()
             .navigationTitle("Piper")
+            //Back button
             .toolbar {
                 ToolbarItem(placement: .topBarLeading) {
                     Button {
@@ -22,18 +34,45 @@ struct ContentView: View {
                         Text("\(Image(systemName: "chevron.backward"))Pets")
                     }
                 }
+                //Plus button
                 ToolbarItem(placement: .topBarTrailing) {
+                    
                     Button{
     
                     } label: {
                         Text ("+")
                             .font(.system(size: 25))
-                            .foregroundStyle(.black)
+                            
     
                     }
+                    .buttonStyle(.borderedProminent)
 
                 }
             }
+            VStack {
+                HStack {
+                    VStack {
+                        Text("7AM")
+                            .font(.title3)
+                            .bold()
+                        Text("11111111")
+                    }
+                    Spacer()
+                    VStack {
+                        Text("1111")
+                        Text("11111111")
+                    }
+                }
+                .padding()
+                .border(.black)
+
+            }
+            .padding()
+            
+            
+            
+            
+            
             
             Spacer()
             
