b2fdd0e1129ec31d1436ced25ef46483809fefff
Yiming Fan <jfan@lcs.on.ca>
2026-02-26 08:52:34 -0500

    Initial Commit

---
+++ From Prototype to Code From Prototype to Code From Prototype to Code From prototype to code exercise/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  From Prototype to Code From Prototype to Code From Prototype to Code From prototype to code exercise
+//
+//  Created by Fan Yiming on 2026-02-26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
